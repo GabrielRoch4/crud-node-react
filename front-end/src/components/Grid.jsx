1fb68c67c27ca1dd9d98ef22152dea835be64a74
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-01-10 15:26:41 -0300

    front recebendo dados dos usuários

--- front-end/src/components/Grid.jsx
+++ front-end/src/components/Grid.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
-// import axios from "axios";
-// import { FaTrash, FaEdit } from "react-icons/fa";
-// import { Toast } from "react-toastify";
+//import axios from "axios";
+//import { Toast } from "react-toastify";
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 const Table = styled.table`
     width: 100%;
@@ -28,7 +29,19 @@ export const Th = styled.th`
     } 
 `;
 
-const Grid = () => {
+export const Tbody = styled.tbody``;
+
+export const Td = styled.td`
+    padding-top: 15px;
+    text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+    width: ${(props) => (props.width ? props.width : "auto")};
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyweb && "display: none"}
+    }
+`;
+
+const Grid = ({ users }) => {
     return (
         <Table>
             <Thead>
@@ -40,6 +53,23 @@ const Grid = () => {
                     <Th></Th>
                 </Tr>
             </Thead>
+            <Tbody>
+                {users.map((item, i) => (
+                    <Tr key={i}>
+                        <Td width="30%">{item.nome}</Td>
+                        <Td width="30%">{item.email}</Td>
+                        <Td width="20%" onlyweb>
+                            {item.telefone}
+                        </Td>
+                        <Td alignCenter width="5%">
+                            <FaEdit />
+                        </Td>
+                        <Td alignCenter width="5%">
+                            <FaTrash />
+                        </Td>
+                    </Tr>
+                ))}
+            </Tbody>
         </Table>
     );
 };
